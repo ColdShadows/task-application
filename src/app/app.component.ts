624d8311d0e7a67a502b06cd1c8ef5c360664a73
Travis K <traviskean04@gmail.com>
2024-11-30 19:30:50 -0500

    Adding Basic Auth Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthButtonComponent } from "./auth-button/auth-button.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthButtonComponent],
+  imports: [RouterOutlet, AuthButtonComponent, UserProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
